2e79900f4b8c7d5788e5710bb2566aaf94980975
greenerous <rlgus33112365@gmail.com>
2022-04-21 12:05:36 +0000

    gDrive commit

---
+++ Programmers/https:  programmers.co.kr learn courses 30 lessons 42884/단속카메라.js
@@ -0,0 +1,8 @@
+https://programmers.co.kr/questions/14636
+
+while 원래 배열.length > 0
+ - 주어진 애들 중에서, 처음에 들어오는 애 (여러 개면 처음으로 나가는 애)를 고름 -> filter로 한놈 뽑아내기
+
+ - 걔의 나가는 애들에 걸치는 모든 애들을 원래 배열에서 삭제
+ - count += 1
+ - continue
